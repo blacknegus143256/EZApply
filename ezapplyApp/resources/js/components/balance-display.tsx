c566762f76402eaf3e131dcba8e0ef59b4360df3
Iam Navs <jcarlounavas@gmail.com>
2025-09-19 16:55:23 +0800

    Added a Credit Display Page

---
+++ ezapplyApp/resources/js/components/balance-display.tsx
@@ -0,0 +1,17 @@
+import { Wallet } from "lucide-react";
+import PermissionGate  from "@/components/PermissionGate";
+
+export default function BalanceDisplay() {
+  const balance = 1+','+200.0;
+
+  return (
+    <PermissionGate role="company">
+    <div className="flex items-center space-x-2 bg-blue-100 dark:bg-neutral-800 px-4 py-2 rounded-lg border border-blue-300 dark:border-neutral-700 shadow-sm">
+      <Wallet className="w-5 h-5 text-blue-600 dark:text-blue-400" />
+      <span className="text-base font-semibold text-blue-700 dark:text-blue-300">
+        ₱ {balance}
+      </span>
+    </div>
+    </PermissionGate>
+  );
+}
--- ezapplyApp/resources/js/layouts/app/app-sidebar-layout.tsx
+++ ezapplyApp/resources/js/layouts/app/app-sidebar-layout.tsx
@@ -4,6 +4,7 @@ import { AppSidebar } from '@/components/app-sidebar';
 import { AppSidebarHeader } from '@/components/app-sidebar-header';
 import { type BreadcrumbItem } from '@/types';
 import { type PropsWithChildren } from 'react';
+import DisplayBalance from '@/components/balance-display';
 
 export default function AppSidebarLayout({ children, breadcrumbs = [] }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
     return (
@@ -11,8 +12,11 @@ export default function AppSidebarLayout({ children, breadcrumbs = [] }: PropsWi
             <AppSidebar />
             <AppContent variant="sidebar" className="overflow-x-hidden">
                 <AppSidebarHeader breadcrumbs={breadcrumbs} />
+                <div className="absolute top-3 right-4">
+                    <DisplayBalance />
+                </div>
                 {children}
-            </AppContent>
+            </AppContent>        
         </AppShell>
     );
 }
